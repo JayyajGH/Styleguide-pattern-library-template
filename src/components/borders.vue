4d7943b0ca76e0f3db5ab622ccd9b29f42377cb2
Jason Harris <jayyaj@hotmail.com>
2017-10-10 20:18:57 +0100

    Use the style-wrapper component

--- src/components/borders.vue
+++ src/components/borders.vue
@@ -13,51 +13,44 @@
 <code>&lt;div class="border--solid border-width--medium border-color--black"&gt;Solid&lt;/div&gt;
 &lt;div class="border--dashed border-width--medium border-color--black"&gt;Dashed&lt;/div&gt;
 &lt;div class="border--dotted border-width--medium border-color--black"&gt;Dotted&lt;/div&gt;</code></pre>
-
     </style-wrapper>
 
-      <h2 class="header2">Uniform border sizes</h2>
-      <p>Define a border size with border-width--thin, border-width--medium or border-width--thick</p>
-
-      <div class="sg-flex sg-sectionborder">
+    <style-wrapper header="Uniform border sizes" description="Define a border size with border-width--thin, border-width--medium or border-width--thick">
+      <div slot="exampleSlot" class="sg-flex">
         <border-example title="Thin" styling="border--solid border-width--thin border-color--black"></border-example>
         <border-example title="Medium" styling="border--solid border-width--medium border-color--black"></border-example>
         <border-example title="Thick" styling="border--solid border-width--thick border-color--black"></border-example>
       </div>
 
-      <div>
-        <pre class="prettyprint">
+      <pre slot="codeSlot" class="prettyprint">
   <code>&lt;div class="border-solid border-width--thin border-color--black"&gt;Thin&lt;/div&gt;
   &lt;div class="border-solid border-width--medium border-color--black"&gt;Medium&lt;/div&gt;
   &lt;div class="border-solid border-width--thick border-color--black"&gt;Thick&lt;/div&gt;</code></pre>
-      </div>
+      </style-wrapper>
 
-      <h2 class="header2">Unequal border sizes</h2>
-      <p>Define a border size with bordertop-width--thin, bordertop-width--medium, bordertop-width--thick,
+      <style-wrapper header="Unequal border sizes" description="Define a border size with bordertop-width--thin, bordertop-width--medium, bordertop-width--thick,
         borderbottom-width--thin, borderbottom-width--medium, borderbottom-width--thick
         borderleft-width--thin, borderleft-width--medium, borderleft-width--thick
-        borderright-width--thin, borderright-width--medium, borderright-width--thick</p>
-
-      <div class="sg-flex sg-sectionborder">
-        <border-example title="Top thin" styling="border--solid bordertop-width--thin border-color--black"></border-example>
-        <border-example title="Top medium" styling="border--solid bordertop-width--medium border-color--black"></border-example>
-        <border-example title="Top thick" styling="border--solid bordertop-width--thick border-color--black"></border-example>
-        <border-example title="Bottom thin" styling="border--solid borderbottom-width--thin border-color--black"></border-example>
-        <border-example title="Bottom medium" styling="border--solid borderbottom-width--medium border-color--black"></border-example>
-        <border-example title="Bottom thick" styling="border--solid borderbottom-width--thick border-color--black"></border-example>
-      </div>
-
-      <div class="sg-flex sg-sectionborder">
-        <border-example title="Left thin" styling="border--solid borderleft-width--thin border-color--black"></border-example>
-        <border-example title="Left medium" styling="border--solid borderleft-width--medium border-color--black"></border-example>
-        <border-example title="Left thick" styling="border--solid borderleft-width--thick border-color--black"></border-example>
-        <border-example title="Right thin" styling="border--solid borderright-width--thin border-color--black"></border-example>
-        <border-example title="Right medium" styling="border--solid borderright-width--medium border-color--black"></border-example>
-        <border-example title="Right thick" styling="border--solid borderright-width--thick border-color--black"></border-example>
-      </div>
-
-      <div>
-        <pre class="prettyprint">
+        borderright-width--thin, borderright-width--medium, borderright-width--thick">
+        <div slot="exampleSlot">
+          <div class="sg-flex">
+            <border-example title="Top thin" styling="border--solid bordertop-width--thin border-color--black"></border-example>
+            <border-example title="Top medium" styling="border--solid bordertop-width--medium border-color--black"></border-example>
+            <border-example title="Top thick" styling="border--solid bordertop-width--thick border-color--black"></border-example>
+            <border-example title="Bottom thin" styling="border--solid borderbottom-width--thin border-color--black"></border-example>
+            <border-example title="Bottom medium" styling="border--solid borderbottom-width--medium border-color--black"></border-example>
+            <border-example title="Bottom thick" styling="border--solid borderbottom-width--thick border-color--black"></border-example>
+          </div>
+          <div class="sg-flex">
+            <border-example title="Left thin" styling="border--solid borderleft-width--thin border-color--black"></border-example>
+            <border-example title="Left medium" styling="border--solid borderleft-width--medium border-color--black"></border-example>
+            <border-example title="Left thick" styling="border--solid borderleft-width--thick border-color--black"></border-example>
+            <border-example title="Right thin" styling="border--solid borderright-width--thin border-color--black"></border-example>
+            <border-example title="Right medium" styling="border--solid borderright-width--medium border-color--black"></border-example>
+            <border-example title="Right thick" styling="border--solid borderright-width--thick border-color--black"></border-example>
+          </div>
+        </div>
+        <pre slot="codeSlot" class="prettyprint">
   <code>&lt;div class="border--solid bordertop-width--thin border-color--black"&gt;Top thin&lt;/div&gt;
   &lt;div class="border--solid bordertop-width--medium border-color--black"&gt;Top medium&lt;/div&gt;
   &lt;div class="border--solid bordertop-width--thick border-color--black"&gt;Top thick&lt;/div&gt;
@@ -70,40 +63,31 @@
   &lt;div class="border--solid borderright-width--thin border-color--black"&gt;Right thin&lt;/div&gt;
   &lt;div class="border--solid borderright-width--medium border-color--black"&gt;Right medium&lt;/div&gt;
   &lt;div class="border--solid borderright-width--thick border-color--black"&gt;Right thick&lt;/div&gt;</code></pre>
-      </div>
+      </style-wrapper>
 
-      <h2 class="header2">Border colour</h2>
-      <p>Define a border colour with border-color--black or border-color--grey</p>
-
-      <div class="sg-flex sg-sectionborder">
-        <border-example title="Black" styling="border--solid border-width--medium border-color--black"></border-example>
-        <border-example title="Grey" styling="border--solid border-width--medium border-color--grey"></border-example>
-      </div>
-
-      <div>
-        <pre class="prettyprint">
+      <style-wrapper header="Border colour" description="Define a border colour with border-color--black or border-color--grey">
+        <div slot="exampleSlot" class="sg-flex">
+          <border-example title="Black" styling="border--solid border-width--medium border-color--black"></border-example>
+          <border-example title="Grey" styling="border--solid border-width--medium border-color--grey"></border-example>
+        </div>
+        <pre slot="codeSlot" class="prettyprint">
   <code>&lt;div class="border--solid border-width--medium border-color--black"&gt;Black&lt;/div&gt;
   &lt;div class="border--solid border-width--medium border-color--grey"&gt;Grey&lt;/div&gt;</code></pre>
-      </div>
+      </style-wrapper>
 
-
-      <h2 class="header2">Border radius</h2>
-      <p>Define a border radius with border-radius--small, border-radius--medium, border-radius--large, border-radius--circle</p>
-
-      <div class="sg-flex sg-sectionborder">
-        <border-example title="Small border radius" styling="border--solid border-width--thin border-color--black border-radius--small"></border-example>
-        <border-example title="Medium border radius" styling="border--solid border-width--thin border-color--black border-radius--medium"></border-example>
-        <border-example title="Large border radius" styling="border--solid border-width--thin border-color--black border-radius--large"></border-example>
-        <border-example title="Circle border radius" styling="border--solid border-width--thin border-color--black border-radius--circle"></border-example>
-      </div>
-
-      <div>
-        <pre class="prettyprint">
+      <style-wrapper header="Border radius" description="Define a border radius with border-radius--small, border-radius--medium, border-radius--large, border-radius--circle">
+        <div slot="exampleSlot" class="sg-flex">
+          <border-example title="Small border radius" styling="border--solid border-width--thin border-color--black border-radius--small"></border-example>
+          <border-example title="Medium border radius" styling="border--solid border-width--thin border-color--black border-radius--medium"></border-example>
+          <border-example title="Large border radius" styling="border--solid border-width--thin border-color--black border-radius--large"></border-example>
+          <border-example title="Circle border radius" styling="border--solid border-width--thin border-color--black border-radius--circle"></border-example>
+        </div>
+        <pre slot="codeSlot" class="prettyprint">
   <code>&lt;div class="border-solid border-width--thin border-color--black border-radius--small"&gt;Small border radius&lt;/div&gt;
   &lt;div class="border-solid border-width--thin border-color--black border-radius--medium"&gt;Medium border radius&lt;/div&gt;
   &lt;div class="border-solid border-width--thin border-color--black border-radius--large"&gt;Large border radius&lt;/div&gt;
   &lt;div class="border-solid border-width--thin border-color--black border-radius--circle"&gt;Circle border radius&lt;/div&gt;</code></pre>
-      </div>
+      </style-wrapper>
   </div>
 </template>
 
@@ -112,7 +96,6 @@
   import BorderExample from '@/components/borderexample'
 
   export default {
-    name: 'borders',
     components: { StyleWrapper, BorderExample }
   }
 </script>
