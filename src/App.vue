252cbda84eac9776c6f650438a0b5cd3920b90f5
Jason Harris <jayyaj@hotmail.com>
2017-10-29 19:32:51 +0000

    Start adding a mobile responsive menu

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         </a>
       </div>
 
-      <ul>
+      <ul class="sg-menu">
         <li class="sg-menu-section sg-paddingleft--small">
             <router-link to="/brand" class="sg-menu-anchor sg-menu-anchor-parent u-inline-block">Brand</router-link>
             <ul>
@@ -44,14 +44,32 @@
       </ul>
     </nav>
 
+    <div class="header">
+      <a href="/" class="">
+        <img src="/static/logo.png" class="sg-logo--small"/>
+      </a>
+      <!--<button class="menu__open js-trigger">Menu</button>-->
+      <div class="hamburger__container js-trigger">
+        <span>MENU</span>
+        <div class=" bar1"></div>
+        <div class=" bar2"></div>
+        <div class=" bar3"></div>
+      </div>
+    </div>
     <router-view class="main"></router-view>
   </main>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app',
+    mounted: function () {
+      $('.js-trigger').click(function () {
+        $(this).toggleClass('change');
+        $('.sidebar').toggleClass('is-active');
+      });
+    }
+  }
 </script>
 
 <style scoped lang="scss">
@@ -59,5 +77,41 @@ export default {
 
   .sg-logo__container {
     margin-bottom: 0;
+    display: none;
+
+    @include respond-to('small and above') {
+      display: block;
+    }
   }
+
+.hamburger__container {
+  height: 100%;
+  cursor: pointer;
+}
+
+.bar1, .bar2, .bar3 {
+    width: 35px;
+    height: 5px;
+    background-color: #333;
+    margin: 6px 0;
+    transition: 0.4s;
+}
+
+/* Rotate first bar */
+.change .bar1 {
+    -webkit-transform: rotate(-45deg) translate(-9px, 6px) ;
+    transform: rotate(-45deg) translate(-9px, 6px) ;
+}
+
+/* Fade out the second bar */
+.change .bar2 {
+    opacity: 0;
+}
+
+/* Rotate last bar */
+.change .bar3 {
+    -webkit-transform: rotate(45deg) translate(-8px, -8px) ;
+    transform: rotate(45deg) translate(-8px, -8px) ;
+}
+
 </style>
