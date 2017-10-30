d9579fe981fb38f3a94ccfffc86afe7853b0cc78
Jason Harris <jayyaj@hotmail.com>
2017-10-30 19:41:15 +0000

    App style tidying

--- src/App.vue
+++ src/App.vue
@@ -51,9 +51,9 @@
       <!--<button class="menu__open js-trigger">Menu</button>-->
       <div class="hamburger__container js-trigger">
         <span>MENU</span>
-        <div class=" bar1"></div>
-        <div class=" bar2"></div>
-        <div class=" bar3"></div>
+        <div class="bar bar1"></div>
+        <div class="bar bar2"></div>
+        <div class="bar bar3"></div>
       </div>
     </div>
     <router-view class="main"></router-view>
@@ -74,44 +74,4 @@
 
 <style scoped lang="scss">
   @import 'static/sass/app.scss';
-
-  .sg-logo__container {
-    margin-bottom: 0;
-    display: none;
-
-    @include respond-to('small and above') {
-      display: block;
-    }
-  }
-
-.hamburger__container {
-  height: 100%;
-  cursor: pointer;
-}
-
-.bar1, .bar2, .bar3 {
-    width: 35px;
-    height: 5px;
-    background-color: #333;
-    margin: 6px 0;
-    transition: 0.4s;
-}
-
-/* Rotate first bar */
-.change .bar1 {
-    -webkit-transform: rotate(-45deg) translate(-9px, 6px) ;
-    transform: rotate(-45deg) translate(-9px, 6px) ;
-}
-
-/* Fade out the second bar */
-.change .bar2 {
-    opacity: 0;
-}
-
-/* Rotate last bar */
-.change .bar3 {
-    -webkit-transform: rotate(45deg) translate(-8px, -8px) ;
-    transform: rotate(45deg) translate(-8px, -8px) ;
-}
-
 </style>
