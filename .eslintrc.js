20b40760bc47f91ffc56694c102ececed82703d2
Jason Harris <jayyaj@hotmail.com>
2017-10-31 18:25:08 +0000

    Correct the comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,7 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    // No semicolons
+    // Enforce semicolons. Always...
     'semi': ['error','always']
   }
 }
