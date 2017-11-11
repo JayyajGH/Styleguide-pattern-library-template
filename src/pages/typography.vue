1e7c2ea027a46a9f07579293e99ca46cfac80a9e
Jason Harris <jayyaj@hotmail.com>
2017-11-11 16:55:35 +0000

    Remove some unnecessary classes

--- src/pages/typography.vue
+++ src/pages/typography.vue
@@ -5,17 +5,17 @@
     <p>We follow a 1.5rem baseline grid for achieving a vertical rhythm on all block-level elements.</p>
 
     <style-wrapper header="Typefaces" description="Our standard typeface is Arial.  Arial is a sans-serif typeface.">
-      <div slot="exampleSlot" class="button--group">
+      <div slot="exampleSlot">
         <p>A B C D E F G H I J K L M N O P Q R S T U V W X Y Z</p>
         <p>a b c d e f g h i j k l m n o p q r s t u v w x y z</p>
         <p>0 1 2 3 4 5 6 7 8 9</p>
       </div>
-      <pre v-highlightjs slot="codeSlot" class="sg-sectionborder sg-code__section">
+      <pre v-highlightjs slot="codeSlot" class="sg-sectionborder sg-code__section sg-margin-bottom--none">
 <code class="html">&lt;p&gt;...&lt;/p&gt;</code></pre>
     </style-wrapper>
 
     <style-wrapper header="Font sizes" description="The font scale contains 7 different font sizes.">
-      <div slot="exampleSlot" class="button--group">
+      <div slot="exampleSlot">
         <p class="header1">{{pangramText}}</p>
         <p class="header2">{{pangramText}}</p>
         <p class="header3">{{pangramText}}</p>
@@ -24,7 +24,7 @@
         <p class="sg-font--small">{{pangramText}}</p>
         <p class="sg-font--smallest">{{pangramText}}</p>
       </div>
-      <pre v-highlightjs slot="codeSlot" class="sg-sectionborder sg-code__section">
+      <pre v-highlightjs slot="codeSlot" class="sg-sectionborder sg-code__section sg-margin-bottom--none">
 <code class="html">&lt;p class="header1"&gt;{{pangramText}}&lt;/p&gt;
 &lt;p class="header2"&gt;{{pangramText}}&lt;/p&gt;
 &lt;p class="header3"&gt;{{pangramText}}&lt;/p&gt;
@@ -35,17 +35,17 @@
     </style-wrapper>
 
     <style-wrapper header="Font weight" description="There are x font weights available to use.">
-      <div slot="exampleSlot" class="button--group">
+      <div slot="exampleSlot">
         <p class="">Normal weight text</p>
         <p class="sg-font--bold">Bold weight text</p>
       </div>
-      <pre v-highlightjs slot="codeSlot" class="sg-sectionborder sg-code__section">
+      <pre v-highlightjs slot="codeSlot" class="sg-sectionborder sg-code__section sg-margin-bottom--none">
 <code class="html">&lt;p&gt;Normal weight text&lt;/p&gt;
 &lt;p class="xyz"&gt;Bold weight text&lt;/p&gt;</code></pre>
     </style-wrapper>
 
     <style-wrapper header="Type tags" description="Text can be set in a number of different semantic html tags. Each tag communicates a different semantic meaning but tags don't carry any aesthetic value.">
-      <div slot="exampleSlot" class="button--group">
+      <div slot="exampleSlot">
         <p>Paragraph tag</p>
         <p><small>Small tag</small></p>
         <p><strong>Strong tag</strong></p>
@@ -53,7 +53,7 @@
         <p><i>Italic tag</i></p>
         <p><em>Emphasis tag</em></p>
       </div>
-      <pre v-highlightjs slot="codeSlot" class="sg-sectionborder  sg-code__section">
+      <pre v-highlightjs slot="codeSlot" class="sg-sectionborder  sg-code__section sg-margin-bottom--none">
 <code class="html">&lt;p&gt;Paragraph tag&lt;/p&gt;
 &lt;small&gt;Small tag&lt;/small&gt;
 &lt;strong&gt;Strong tag&lt;/strong&gt;
@@ -64,12 +64,12 @@
 
 
     <style-wrapper header="Transformation classes" description="Transform text with text transformation classes">
-      <div slot="exampleSlot" class="button--group">
+      <div slot="exampleSlot">
         <p class="u-text-lowercase">Lowercase text</p>
         <p class="u-text-uppercase">Uppercase text</p>
         <p class="u-text-capitalize">Capitalized text</p>
       </div>
-      <pre v-highlightjs slot="codeSlot" class="sg-sectionborder  sg-code__section">
+      <pre v-highlightjs slot="codeSlot" class="sg-sectionborder  sg-code__section sg-margin-bottom--none">
 <code class="html">&lt;p class="u-text-lowercase"&gt;Lowercase text&lt;/p&gt;
 &lt;p class="u-text-uppercase"&gt;Uppercase text&lt;/p&gt;
 &lt;p class="u-text-capitalize"&gt;Capitalized text&lt;/p&gt;</code></pre>
