515f94b7f3b8bdc5066ccc621f601147540d9428
Jason Harris <jayyaj@hotmail.com>
2017-11-23 13:38:11 +0000

    Correct the es6 linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,10 @@ module.exports = {
   parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module',
-    esversion: 6
+    ecmaVersion: 6
   },
   env: {
-    browser: true,
+    browser: true
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
   extends: 'standard',
