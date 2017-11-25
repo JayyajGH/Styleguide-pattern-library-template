a770ecc01dae14fdb5323cbbb396e571697eaab4
Jason Harris <jayyaj@hotmail.com>
2017-11-25 17:53:23 +0000

    Add a description slot to the style wrapper component
    This is to allow a more detailed description if required.  Both the property and slot are optional

--- src/components/stylewrapper.vue
+++ src/components/stylewrapper.vue
@@ -3,6 +3,8 @@
     <header>
       <h2 class="header2">{{header}}</h2>
     </header>
+    <!-- A simple description can be provided via the description property or a more complex description can be provided using the description slot -->
+    <slot name="descriptionSlot"></slot>
     <p>{{description}}</p>
     <div class="sg-sectionborder sg-padding--large sg-margin-bottom--none">
       <slot name="exampleSlot"></slot>
