7118e36f375a7ea7ee0d0e4c35a9f83dddbb2ad9
Jason Harris <jayyaj@hotmail.com>
2017-11-25 17:12:16 +0000

    Add some blurb about what a pattern library is

--- src/pages/patternlibrary.vue
+++ src/pages/patternlibrary.vue
@@ -3,6 +3,15 @@
 
     <heading title="Pattern Library" :isMainHeading=true></heading>
 
+    <p>This pattern library describes the available collection of user interfae elements.</p>
+    <p>This allows us: </p>
+    <ul>
+      <li>To achieve a consistent look and feel across the site</li>
+      <li>Provide a common vocabularly for each user</li>
+      <li>Provide guidance on the correct use of each element</li>
+      <li>Provide a single point of reference to see the available patterns</li>
+    </ul>
+
   </div>
 </template>
 
