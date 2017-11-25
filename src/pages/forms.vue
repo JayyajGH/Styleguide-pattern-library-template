f6a1e33515ddd7c8909aecc5e347324ff7d5026d
Jason Harris <jayyaj@hotmail.com>
2017-11-25 21:36:05 +0000

    Add some dummy form examples

--- src/pages/forms.vue
+++ src/pages/forms.vue
@@ -2,9 +2,44 @@
   <div id="forms" class="spa-page">
     <heading title="Forms"></heading>
 
-    <h2>Form fields</h2>
+    <section>
+      <header>
+        <h2 class="header2">Form fields</h2>
+      </header>
 
-    <h2>Placeholder text</h2>
+      <form>
+        <fieldset>
+          <legend>Name</legend>
+          Forename:
+          <input type="text" name="forename"><br>
+          Surname:
+          <input type="text" name="surname"><br>
+        </fieldset>
+
+        <fieldset>
+          <legend>Gender</legend>
+          <input type="radio" name="gender" value="male" checked>Male
+          <input type="radio" name="gender" value="female">Female
+          <input type="radio" name="gender" value="other">Other
+        </fieldset>
+
+        <fieldset>
+          <legend>Transport</legend>
+          <input type="checkbox" name="vehicle1" value="Bike"> I have a bike
+          <input type="checkbox" name="vehicle2" value="Car"> I have a car
+        </fieldset>
+
+        <fieldset>
+          <legend>Model</legend>
+          <select name="cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="fiat">Fiat</option>
+            <option value="audi">Audi</option>
+          </select>
+        </fieldset>
+      </form>
+    </section>
 
   </div>
 </template>
