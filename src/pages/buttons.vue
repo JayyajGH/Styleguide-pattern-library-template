fa24ce15b23dc2dd0cb1854a1361c3aca9e5c190
Jason Harris <jayyaj@hotmail.com>
2017-11-25 11:46:33 +0000

    Make the HTML more semantic

--- src/pages/buttons.vue
+++ src/pages/buttons.vue
@@ -4,83 +4,83 @@
 
     <p>These styles will work on the button element, an input element with a type of button and an anchor styled as a button</p>
 
-    <h2 class="header2">Button sizes</h2>
-    <p>Define a button size with button--small, button--medium or button--large</p>
+    <section>
+      <h2 class="header2">Button sizes</h2>
+      <p>Define a button size with button--small, button--medium or button--large</p>
 
-    <div class="button--group sg-flex sg-flex-button sg-sectionborder sg-padding--large u-flex sg-margin-bottom--none">
-      <span v-for="bsize in buttonSize" class="sg-margin-bottom--medium">
-        <button type="button" class="button button--simple button--primary" :class=bsize.styling>{{bsize.name}}</button>
-      </span>
-    </div>
+      <div class="button--group sg-flex sg-flex-button sg-sectionborder sg-padding--large u-flex sg-margin-bottom--none">
+        <span v-for="bsize in buttonSize" class="sg-margin-bottom--medium">
+          <button type="button" class="button button--simple button--primary" :class=bsize.styling>{{bsize.name}}</button>
+        </span>
+      </div>
 
-    <div>
       <code-snippet>
         <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(buttonSize, 'button button--simple button--primary')" class="sg-margin-bottom--none"></div>
       </code-snippet>
-    </div>
+    </section>
 
-    <h2 class="header2">Button importance</h2>
-    <p>Define a buttons importance with button--primary, button--secondary or button--tertiary</p>
+    <section>
+      <h2 class="header2">Button importance</h2>
+      <p>Define a buttons importance with button--primary, button--secondary or button--tertiary</p>
 
-     <div class="button--group sg-flex sg-flex-button sg-sectionborder sg-padding--large u-flex sg-margin-bottom--none">
-       <span v-for="bimportant in buttonImportance" class="sg-margin-bottom--medium">
-         <button type="button" class="button button--simple button--medium" :class=bimportant.styling>{{bimportant.name}}</button>
-       </span>
-     </div>
+       <div class="button--group sg-flex sg-flex-button sg-sectionborder sg-padding--large u-flex sg-margin-bottom--none">
+         <span v-for="bimportant in buttonImportance" class="sg-margin-bottom--medium">
+           <button type="button" class="button button--simple button--medium" :class=bimportant.styling>{{bimportant.name}}</button>
+         </span>
+       </div>
 
-     <div>
        <code-snippet>
          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(buttonImportance, 'button button--simple button--medium')" class="sg-margin-bottom--none"></div>
        </code-snippet>
-     </div>
+    </section>
 
-    <h2 class="header2">Button width</h2>
-    <p>Define the width of a button to a set size with button--halfwidth or button--fullwidth</p>
+    <section>
+      <h2 class="header2">Button width</h2>
+      <p>Define the width of a button to a set size with button--halfwidth or button--fullwidth</p>
 
-    <div class="button--group sg-sectionborder sg-padding--large sg-margin-bottom--none">
-      <div v-for="bwidth in buttonWidth">
-        <button type="button" class="button button--simple button--primary button--medium" :class=bwidth.styling>{{bwidth.name}}</button>
+      <div class="button--group sg-sectionborder sg-padding--large sg-margin-bottom--none">
+        <div v-for="bwidth in buttonWidth">
+          <button type="button" class="button button--simple button--primary button--medium" :class=bwidth.styling>{{bwidth.name}}</button>
+        </div>
       </div>
-    </div>
 
-    <div>
       <code-snippet>
         <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(buttonWidth, 'button button--simple button--primary button--medium')" class="sg-margin-bottom--none"></div>
       </code-snippet>
-    </div>
+    </section>
 
-    <h2 class="header2">Button Styles</h2>
-    <p>Define the style of a button with button--simple or button--ghost.  All other options can be applied to each style.</p>
+    <section>
+      <h2 class="header2">Button Styles</h2>
+      <p>Define the style of a button with button--simple or button--ghost.  All other options can be applied to each style.</p>
 
-    <div class="button--group sg-flex sg-flex-button sg-sectionborder sg-padding--large u-flex sg-margin-bottom--none">
-      <span v-for="bstyle in buttonStyle" class="sg-margin-bottom--medium">
-        <button type="button" class="button button--medium" :class=bstyle.styling>{{bstyle.name}}</button>
-      </span>
-    </div>
+      <div class="button--group sg-flex sg-flex-button sg-sectionborder sg-padding--large u-flex sg-margin-bottom--none">
+        <span v-for="bstyle in buttonStyle" class="sg-margin-bottom--medium">
+          <button type="button" class="button button--medium" :class=bstyle.styling>{{bstyle.name}}</button>
+        </span>
+      </div>
 
-    <div>
       <code-snippet>
         <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(buttonStyle, 'button button--medium')" class="sg-margin-bottom--none"></div>
       </code-snippet>
-    </div>
+    </section>
 
-    <h2 class="header2">Button Grouping</h2>
-    <p>Set a grouping container for buttons.  This will add the correct padding etc.</p>
+    <section>
+      <h2 class="header2">Button Grouping</h2>
+      <p>Set a grouping container for buttons.  This will add the correct padding etc.</p>
 
-    <div class="button--group sg-flex sg-flex-button sg-sectionborder sg-padding--large u-flex sg-margin-bottom--none">
-      <span class="sg-margin-bottom--medium">
-        <button type="button" class="button button--simple button--primary button--medium">Button 1</button>
-      </span>
-      <span class="sg-margin-bottom--medium">
-        <button type="button" class="button button--simple button--primary button--medium">Button 2</button>
-      </span>
-    </div>
+      <div class="button--group sg-flex sg-flex-button sg-sectionborder sg-padding--large u-flex sg-margin-bottom--none">
+        <span class="sg-margin-bottom--medium">
+          <button type="button" class="button button--simple button--primary button--medium">Button 1</button>
+        </span>
+        <span class="sg-margin-bottom--medium">
+          <button type="button" class="button button--simple button--primary button--medium">Button 2</button>
+        </span>
+      </div>
 
-    <div>
       <code-snippet slot="codeSlot">
         <div slot="codeSnippetSlot" v-html="buttonGroupingCodeSnippet" class="sg-margin-bottom--none"></div>
       </code-snippet>
-    </div>
+    </section>
 
   </div>
 </template>
