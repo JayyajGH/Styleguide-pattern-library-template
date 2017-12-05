ae53d868dd94cd82e27edf8786d393acfb41e6ee
Jason Harris <jayyaj@hotmail.com>
2017-12-05 19:00:56 +0000

    Refactor to allow a prefix to each line and simplify the class logic

--- src/components/mixins/codestring.js
+++ src/components/mixins/codestring.js
@@ -1,19 +1,30 @@
+
+// Construct the elements class definition if required.
+// This can be a combination of the style passed in with the element and also any common styles
+function generateRequiredClasses (style, additionalClasses) {
+  let styling = style;
+
+  if (additionalClasses) {
+    styling += styling ? ` ${additionalClasses}` : `${additionalClasses}`;
+  }
+
+  return styling;
+}
+
 export default {
   methods: {
-    returnFormattedCodeString: function (snippetList, additionalClasses) {
+    returnFormattedCodeString: function (snippetList, additionalClasses, elementPrefix) {
       let codeSnippetList = [];
       let codeSnippetString = ``;
 
       if (snippetList) {
         snippetList.forEach(value => {
-          // Construct the elements class definition if required.
-          // This can be a combination of the style passed in with the element and also any common styles
-          let styling = `${value.style}${additionalClasses ? (' ' + additionalClasses) : ''}`;
+          let styling = generateRequiredClasses(value.style, additionalClasses);
           let classDefinition = styling ? ` class="${styling}"` : ``;
 
           const {element} = value;
 
-          codeSnippetList.push(`&lt;${element}${classDefinition}&gt;${value.content}&lt;/${element}&gt;`);
+          codeSnippetList.push(`${elementPrefix || ''}&lt;${element}${classDefinition}&gt;${value.content}&lt;/${element}&gt;`);
         });
 
         codeSnippetString = codeSnippetList.join('\n');
