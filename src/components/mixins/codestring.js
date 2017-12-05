db0f1b5108a6a2d10d67a87ca8445d67c53d6636
Jason Harris <jayyaj@hotmail.com>
2017-12-05 10:57:02 +0000

    Some refactoring to tidy up the code

--- src/components/mixins/codestring.js
+++ src/components/mixins/codestring.js
@@ -1,22 +1,25 @@
 export default {
   methods: {
     returnFormattedCodeString: function (snippetList, additionalClasses) {
-      let templateString = ``;
+      let codeSnippetList = [];
+      let codeSnippetString = ``;
 
       if (snippetList) {
         snippetList.forEach(value => {
           // Construct the elements class definition if required.
           // This can be a combination of the style passed in with the element and also any common styles
           let styling = `${value.style}${additionalClasses ? (' ' + additionalClasses) : ''}`;
-          let classDefinition = `${styling ? ` class="${styling}"` : ``}`;
+          let classDefinition = styling ? ` class="${styling}"` : ``;
 
           const {element} = value;
 
-          templateString += `&lt;${element}${classDefinition}&gt;${value.content}&lt;/${element}&gt;\n`;
+          codeSnippetList.push(`&lt;${element}${classDefinition}&gt;${value.content}&lt;/${element}&gt;`);
         });
+
+        codeSnippetString = codeSnippetList.join('\n');
       }
 
-      return templateString;
+      return codeSnippetString;
     }
   }
 };
