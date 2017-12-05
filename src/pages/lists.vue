a45fab3cc5bb3044be99fb50250ec437a937a980
Jason Harris <jayyaj@hotmail.com>
2017-12-05 19:14:29 +0000

    Use a standard method of generating the code snippet

--- src/pages/lists.vue
+++ src/pages/lists.vue
@@ -33,14 +33,19 @@
   import StyleWrapper from '@/components/stylewrapper';
   import Heading from '@/components/heading';
   import CodeSnippet from '@/components/codesnippet';
+  import returnFormattedCodeString from '@/components/mixins/codestring';
+  import ElementExample from '@/components/classes/elementexample';
 
   export default {
     components: { StyleWrapper, Heading, CodeSnippet },
+    mixins: [returnFormattedCodeString],
     computed: {
       listCodeSnippet: function () {
-        return `${this.list.listDisc}\n  ${this.list.listItemExample1}\n  ${this.list.listItemExample2}\n${this.list.listEndList}\n
-${this.list.listCircle}\n  ${this.list.listItemExample1}\n  ${this.list.listItemExample2}\n${this.list.listEndList}\n
-${this.list.listNone}\n  ${this.list.listItemExample1}\n  ${this.list.listItemExample2}\n${this.list.listEndList}`;
+        const items = this.returnFormattedCodeString(this.listItems, '', '&nbsp;&nbsp;');
+
+        return `${this.list.listDisc}\n${items}\n${this.list.listEndList}\n
+${this.list.listCircle}\n${items}}\n${this.list.listEndList}\n
+${this.list.listNone}\n${items}}\n${this.list.listEndList}`;
       }
     },
     data: function () {
@@ -49,10 +54,12 @@ ${this.list.listNone}\n  ${this.list.listItemExample1}\n  ${this.list.listItemEx
           listDisc: '&lt;ul class="list list--disc"&gt;',
           listCircle: '&lt;ul class="list list--circle"&gt;',
           listNone: '&lt;ul class="list list--none"&gt;',
-          listItemExample1: '&lt;li&gt;Item 1&lt;/li&gt;',
-          listItemExample2: '&lt;li&gt;Item 2&lt;/li&gt;',
           listEndList: '&lt;/ul&gt;'
-        }
+        },
+        listItems: [
+          new ElementExample('Item 1', '', 'li'),
+          new ElementExample('Item 2', '', 'li')
+        ]
       };
     }
   };
