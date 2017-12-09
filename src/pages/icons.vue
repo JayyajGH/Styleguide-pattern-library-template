441d6fbeeb8318d1c334b59d30f7c043aa46916e
Jason Harris <jayyaj@hotmail.com>
2017-12-09 11:09:49 +0000

    Add some placeholders for icon headings

--- src/pages/icons.vue
+++ src/pages/icons.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="icons" class="spa-page">
     <heading title="Icons"></heading>
+
+    <h2 class="header2">Social Icons</h2>
+    <h2 class="header2">General</h2>
   </div>
 </template>
 
