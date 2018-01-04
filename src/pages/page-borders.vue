0aaf0d8fcb4d9429e2bccd5496396c742c30f748
Jason Harris <jayyaj@hotmail.com>
2018-01-04 13:49:30 +0000

    Refactor to store the section title and description in the object.  This will allow me to refactor this file to be much tidier and only have one element which loops through each section.

--- src/pages/page-borders.vue
+++ src/pages/page-borders.vue
@@ -3,61 +3,61 @@
 
     <heading title="Borders"></heading>
     <!-- Border styles -->
-    <style-wrapper header="Border styles" description="Define the style of the border">
+    <style-wrapper :header=borderStyle.title :description=borderStyle.description>
       <div slot="exampleSlot" class="sg-flex--main-axis-spacearound sg-flex--cross-axis-center sg-flex-border u-flex">
-          <border-example v-for="bstyle in borderStyle" :key=bstyle.content :title=bstyle.content :styling="[bstyle.style,'border-width--medium border-color--black']"></border-example>
+          <border-example v-for="bstyle in borderStyle.elements" :key=bstyle.content :title=bstyle.content :styling="[bstyle.style,'border-width--medium border-color--black']"></border-example>
       </div>
       <div slot="codeSlot">
         <code-snippet>
-          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(borderStyle, 'border-width--medium border-color--black')" class="sg-margin-bottom--none"></div>
+          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(borderStyle.elements, 'border-width--medium border-color--black')" class="sg-margin-bottom--none"></div>
         </code-snippet>
       </div>
     </style-wrapper>
 
     <!-- Uniform border sizes -->
-    <style-wrapper header="Uniform border sizes" description="Define the width of the border where the width required is the same on all sides">
+    <style-wrapper :header=borderSize.title :description=borderSize.description>
       <div slot="exampleSlot" class="sg-flex--main-axis-spacearound sg-flex--cross-axis-center sg-flex-border u-flex">
-        <border-example v-for="bsize in borderSize" :key=bsize.content :title=bsize.content :styling="[bsize.style,'border--solid border-color--black']"></border-example>
+        <border-example v-for="bsize in borderSize.elements" :key=bsize.content :title=bsize.content :styling="[bsize.style,'border--solid border-color--black']"></border-example>
       </div>
       <div slot="codeSlot">
         <code-snippet>
-          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(borderSize, 'border--solid border-color--black')" class="sg-margin-bottom--none"></div>
+          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(borderSize.elements, 'border--solid border-color--black')" class="sg-margin-bottom--none"></div>
         </code-snippet>
       </div>
     </style-wrapper>
 
     <!-- Unequal border sizes -->
-    <style-wrapper header="Unequal border sizes" description="Define the width of the border on individual sides">
+    <style-wrapper :header=borderSizeUnequal.title :description=borderSizeUnequal.description>
       <div slot="exampleSlot" class="sg-flex--main-axis-spacearound sg-flex--cross-axis-center sg-flex-border sg-flex--wrap u-flex">
-        <border-example v-for="bsizeueq in borderSizeUnequal" :key=bsizeueq.content :title=bsizeueq.content :styling="[bsizeueq.style,'border--solid border-color--black']"></border-example>
+        <border-example v-for="bsizeueq in borderSizeUnequal.elements" :key=bsizeueq.content :title=bsizeueq.content :styling="[bsizeueq.style,'border--solid border-color--black']"></border-example>
       </div>
       <div slot="codeSlot">
         <code-snippet>
-          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(borderSizeUnequal, 'border--solid border-color--black')" class="sg-margin-bottom--none"></div>
+          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(borderSizeUnequal.elements, 'border--solid border-color--black')" class="sg-margin-bottom--none"></div>
         </code-snippet>
       </div>
     </style-wrapper>
 
     <!-- Border colour -->
-    <style-wrapper header="Border colour" description="Define the colour of the border">
+    <style-wrapper :header=borderColour.title :description=borderColour.description>
       <div slot="exampleSlot" class="sg-flex--main-axis-spacearound sg-flex--cross-axis-center sg-flex-border u-flex">
-        <border-example v-for="bcolour in borderColour" :key=bcolour.content :title=bcolour.content :styling="[bcolour.style,'border--solid border-width--medium']"></border-example>
+        <border-example v-for="bcolour in borderColour.elements" :key=bcolour.content :title=bcolour.content :styling="[bcolour.style,'border--solid border-width--medium']"></border-example>
       </div>
       <div slot="codeSlot">
         <code-snippet>
-          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(borderColour, 'border--solid border-width--medium')" class="sg-margin-bottom--none"></div>
+          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(borderColour.elements, 'border--solid border-width--medium')" class="sg-margin-bottom--none"></div>
         </code-snippet>
       </div>
     </style-wrapper>
 
     <!-- Border radius -->
-    <style-wrapper header="Border radius" description="Define the border radius of the border">
+    <style-wrapper :header=borderRadius.title :description=borderRadius.description>
       <div slot="exampleSlot" class="sg-flex--main-axis-spacearound sg-flex--cross-axis-center sg-flex-border u-flex">
-        <border-example v-for="bradius in borderRadius" :key=bradius.content :title=bradius.content :styling="[bradius.style,'border--solid border-width--thin border-color--black']"></border-example>
+        <border-example v-for="bradius in borderRadius.elements" :key=bradius.content :title=bradius.content :styling="[bradius.style,'border--solid border-width--thin border-color--black']"></border-example>
       </div>
       <div slot="codeSlot">
         <code-snippet>
-          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(borderRadius, 'border-solid border-width--thin border-color--black')" class="sg-margin-bottom--none"></div>
+          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(borderRadius.elements, 'border-solid border-width--thin border-color--black')" class="sg-margin-bottom--none"></div>
         </code-snippet>
       </div>
     </style-wrapper>
@@ -77,47 +77,64 @@
     mixins: [returnFormattedCodeString],
     data: function () {
       return {
-        borderStyle: [
-          new ElementExample('Solid', 'border--solid', 'div'),
-          new ElementExample('Dashed', 'border--dashed', 'div'),
-          new ElementExample('Dotted', 'border--dotted', 'div')
-        ],
-        borderSize: [
-          new ElementExample('Thin', 'border-width--thin', 'div'),
-          new ElementExample('Medium', 'border-width--medium', 'div'),
-          new ElementExample('Thick', 'border-width--thick', 'div')
-        ],
-        borderSizeUnequal: [
-          new ElementExample('Top Thin', 'bordertop-width--thin', 'div'),
-          new ElementExample('Top Medium', 'bordertop-width--medium', 'div'),
-          new ElementExample('Top Thick', 'bordertop-width--thick', 'div'),
-          new ElementExample('Bottom Thin', 'borderbottom-width--thin', 'div'),
-          new ElementExample('Bottom Medium', 'borderbottom-width--medium', 'div'),
-          new ElementExample('Bottom Thick', 'borderbottom-width--thick', 'div'),
-          new ElementExample('Left Thin', 'borderleft-width--thin', 'div'),
-          new ElementExample('Left Medium', 'borderleft-width--medium', 'div'),
-          new ElementExample('Left Thick', 'borderleft-width--thick', 'div'),
-          new ElementExample('Right Thin', 'borderright-width--thin', 'div'),
-          new ElementExample('Right Medium', 'borderright-width--medium', 'div'),
-          new ElementExample('Right Thick', 'borderright-width--thick', 'div')
-        ],
-        borderColour: [
-          new ElementExample('Black', 'border-color--black', 'div'),
-          new ElementExample('Grey', 'border-color--grey', 'div')
-        ],
-        borderRadius: [
-          new ElementExample('Small', 'border-radius--small', 'div'),
-          new ElementExample('Medium', 'border-radius--medium', 'div'),
-          new ElementExample('Large', 'border-radius--large', 'div'),
-          new ElementExample('Circle', 'border-radius--circle', 'div')
-        ]
+        borderStyle: {
+          title: 'Border styles',
+          description: 'Define the style of the border',
+          elements: [
+            new ElementExample('Solid', 'border--solid', 'div'),
+            new ElementExample('Dashed', 'border--dashed', 'div'),
+            new ElementExample('Dotted', 'border--dotted', 'div')
+          ]
+        },
+        borderSize: {
+          title: 'Uniform border sizes',
+          description: 'Define the width of the border where the width required is the same on all sides',
+          elements: [
+            new ElementExample('Thin', 'border-width--thin', 'div'),
+            new ElementExample('Medium', 'border-width--medium', 'div'),
+            new ElementExample('Thick', 'border-width--thick', 'div')
+          ]
+        },
+        borderSizeUnequal: {
+          title: 'Unequal border sizes',
+          description: 'Define the width of the border on individual sides',
+          elements: [
+            new ElementExample('Top Thin', 'bordertop-width--thin', 'div'),
+            new ElementExample('Top Medium', 'bordertop-width--medium', 'div'),
+            new ElementExample('Top Thick', 'bordertop-width--thick', 'div'),
+            new ElementExample('Bottom Thin', 'borderbottom-width--thin', 'div'),
+            new ElementExample('Bottom Medium', 'borderbottom-width--medium', 'div'),
+            new ElementExample('Bottom Thick', 'borderbottom-width--thick', 'div'),
+            new ElementExample('Left Thin', 'borderleft-width--thin', 'div'),
+            new ElementExample('Left Medium', 'borderleft-width--medium', 'div'),
+            new ElementExample('Left Thick', 'borderleft-width--thick', 'div'),
+            new ElementExample('Right Thin', 'borderright-width--thin', 'div'),
+            new ElementExample('Right Medium', 'borderright-width--medium', 'div'),
+            new ElementExample('Right Thick', 'borderright-width--thick', 'div')
+          ]
+        },
+        borderColour: {
+          title: 'Border colour',
+          description: 'Define the colour of the border',
+          elements: [
+            new ElementExample('Black', 'border-color--black', 'div'),
+            new ElementExample('Grey', 'border-color--grey', 'div')
+          ]
+        },
+        borderRadius: {
+          title: 'Border radius',
+          description: 'Define the border radius of the border',
+          elements: [
+            new ElementExample('Small', 'border-radius--small', 'div'),
+            new ElementExample('Medium', 'border-radius--medium', 'div'),
+            new ElementExample('Large', 'border-radius--large', 'div'),
+            new ElementExample('Circle', 'border-radius--circle', 'div')
+          ]
+        }
       };
     }
   };
 </script>
-
-<style lang="scss">
-</style>
 
 <style scoped lang="scss">
   @import 'static/sass/abstracts/_abstracts.scss';
