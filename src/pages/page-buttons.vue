136a97eca8db58fd31e797fb7da88f9e864231fd
Jason Harris <jayyaj@hotmail.com>
2018-01-07 11:43:32 +0000

    Refactoring to be data driven

--- src/pages/page-buttons.vue
+++ src/pages/page-buttons.vue
@@ -4,7 +4,7 @@
 
     <p>These styles will work on the button element, an input element with a type of button and an anchor styled as a button</p>
 
-    <style-wrapper header="Button sizes">
+    <style-wrapper :header=buttonSize.title>
       <div slot="descriptionSlot">
         <p>Use these classes to define the button size.  The button sizes should be used as follows:</p>
         <ul class="list">
@@ -14,18 +14,18 @@
         </ul>
       </div>
       <div slot="exampleSlot" class="sg-flex--main-axis-spacearound sg-flex--cross-axis-center sg-flex-button u-flex sg-margin-bottom--none">
-        <span v-for="bsize in buttonSize" class="sg-margin-bottom--medium">
-          <button type="button" class="button button--simple button--primary" :class=bsize.style>{{bsize.content}}</button>
+        <span v-for="bsize in buttonSize.elements" class="sg-margin-bottom--medium">
+          <button type="button" :class="[bsize.style, buttonSize.commonClasses]">{{bsize.content}}</button>
         </span>
       </div>
       <div slot="codeSlot">
         <code-snippet>
-          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(buttonSize, 'button button--simple button--primary')" class="sg-margin-bottom--none"></div>
+          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(buttonSize.elements, buttonSize.commonClasses)" class="sg-margin-bottom--none"></div>
         </code-snippet>
       </div>
     </style-wrapper>
 
-    <style-wrapper header="Button importance">
+    <style-wrapper :header=buttonImportance.title>
       <div slot="descriptionSlot">
         <p>Use these classes to define a buttons importance. The button importance should be used as follows:</p>
         <ul class="list">
@@ -35,34 +35,34 @@
         </ul>
       </div>
       <div slot="exampleSlot" class="sg-flex--main-axis-spacearound sg-flex--cross-axis-center sg-flex-button u-flex sg-margin-bottom--none">
-        <span v-for="bimportant in buttonImportance" class="sg-margin-bottom--medium">
-          <button type="button" class="button button--simple button--medium" :class=bimportant.style>{{bimportant.content}}</button>
+        <span v-for="bimportant in buttonImportance.elements" class="sg-margin-bottom--medium">
+          <button type="button" :class="[bimportant.style, buttonImportance.commonClasses]">{{bimportant.content}}</button>
         </span>
       </div>
       <div slot="codeSlot">
         <code-snippet>
-          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(buttonImportance, 'button button--simple button--medium')" class="sg-margin-bottom--none"></div>
+          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(buttonImportance.elements, buttonImportance.commonClasses)" class="sg-margin-bottom--none"></div>
         </code-snippet>
       </div>
     </style-wrapper>
 
-    <style-wrapper header="Button width">
+    <style-wrapper :header=buttonWidth.title>
       <div slot="descriptionSlot">
         <p>Use these classes to define the width of a button</p>
       </div>
       <div slot="exampleSlot" class="sg-margin-bottom--none">
-        <div v-for="bwidth in buttonWidth" class="sg-margin-bottom--medium">
-          <button type="button" class="button button--simple button--primary button--medium" :class=bwidth.style>{{bwidth.content}}</button>
+        <div v-for="bwidth in buttonWidth.elements" class="sg-margin-bottom--medium">
+          <button type="button" :class="[bwidth.style, buttonWidth.commonClasses]">{{bwidth.content}}</button>
         </div>
       </div>
       <div slot="codeSlot">
         <code-snippet>
-          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(buttonWidth, 'button button--simple button--primary button--medium')" class="sg-margin-bottom--none"></div>
+          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(buttonWidth.elements, buttonWidth.commonClasses)" class="sg-margin-bottom--none"></div>
         </code-snippet>
       </div>
     </style-wrapper>
 
-    <style-wrapper header="Button styles">
+    <style-wrapper :header=buttonStyle.title>
       <div slot="descriptionSlot">
         <p>Use these classes to define the style of the button. All other options can be applied to each style.</p>
         <ul class="list">
@@ -71,24 +71,24 @@
         </ul>
       </div>
       <div slot="exampleSlot" class="sg-flex--main-axis-spacearound sg-flex--cross-axis-center sg-flex-button u-flex sg-margin-bottom--none">
-        <span v-for="bstyle in buttonStyle" class="sg-margin-bottom--medium">
-          <button type="button" class="button button--medium" :class=bstyle.style>{{bstyle.content}}</button>
+        <span v-for="bstyle in buttonStyle.elements" class="sg-margin-bottom--medium">
+          <button type="button" :class="[bstyle.style, buttonStyle.commonClasses]">{{bstyle.content}}</button>
         </span>
       </div>
       <div slot="codeSlot">
         <code-snippet>
-          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(buttonStyle, 'button button--medium')" class="sg-margin-bottom--none"></div>
+          <div slot="codeSnippetSlot" v-html="returnFormattedCodeString(buttonStyle.elements, buttonStyle.commonClasses)" class="sg-margin-bottom--none"></div>
         </code-snippet>
       </div>
     </style-wrapper>
 
-    <style-wrapper header="Button grouping" description="Set a grouping container for buttons.  This will add the correct padding etc.">
+    <style-wrapper :header=buttonGroup.title :description=buttonGroup.description>
       <div slot="exampleSlot" class="sg-flex--main-axis-spacearound sg-flex--cross-axis-center u-flex sg-margin-bottom--none">
         <span class="sg-margin-bottom--medium">
-          <button type="button" class="button button--simple button--primary button--medium">Button</button>
+          <button type="button" :class=buttonGroup.commonClasses>Button</button>
         </span>
         <span class="sg-margin-bottom--medium">
-          <button type="button" class="button button--simple button--primary button--medium">Button</button>
+          <button type="button" :class=buttonGroup.commonClasses>Button</button>
         </span>
       </div>
       <div slot="codeSlot">
@@ -113,35 +113,56 @@
     mixins: [returnFormattedCodeString],
     computed: {
       buttonGroupingCodeSnippet: function () {
-        const buttons = this.returnFormattedCodeString(this.buttonGroup, 'button button--simple button--primary button--medium', '&nbsp;&nbsp;');
+        const buttons = this.returnFormattedCodeString(this.buttonGroup.elements, 'button button--simple button--primary button--medium', '&nbsp;&nbsp;');
 
         return `&lt;div class="button--group"&gt;\n${buttons}\n&lt;/div&gt;`;
       }
     },
     data: function () {
       return {
-        buttonSize: [
-          new ElementExample('Small button', 'button--small', 'button'),
-          new ElementExample('Medium button', 'button--medium', 'button'),
-          new ElementExample('Large button', 'button--large', 'button')
-        ],
-        buttonImportance: [
-          new ElementExample('Primary button', 'button--primary', 'button'),
-          new ElementExample('Secondary button', 'button--secondary', 'button'),
-          new ElementExample('Tertiary button', 'button--tertiary', 'button')
-        ],
-        buttonWidth: [
-          new ElementExample('Half-width button', 'button--halfwidth', 'button'),
-          new ElementExample('Full-width button', 'button--fullwidth', 'button')
-        ],
-        buttonStyle: [
-          new ElementExample('Simple button', 'button--simple button--primary', 'button'),
-          new ElementExample('Ghost button', 'button--ghost button-ghost--primary', 'button')
-        ],
-        buttonGroup: [
-          new ElementExample('Button 1', '', 'button'),
-          new ElementExample('Button 2', '', 'button')
-        ]
+        buttonSize: {
+          title: 'Button sizes',
+          commonClasses: 'button button--simple button--primary',
+          elements: [
+            new ElementExample('Small button', 'button--small', 'button'),
+            new ElementExample('Medium button', 'button--medium', 'button'),
+            new ElementExample('Large button', 'button--large', 'button')
+          ]
+        },
+        buttonImportance: {
+          title: 'Button importance',
+          commonClasses: 'button button--simple button--medium',
+          elements: [
+            new ElementExample('Primary button', 'button--primary', 'button'),
+            new ElementExample('Secondary button', 'button--secondary', 'button'),
+            new ElementExample('Tertiary button', 'button--tertiary', 'button')
+          ]
+        },
+        buttonWidth: {
+          title: 'Button width',
+          commonClasses: 'button button--simple button--primary button--medium',
+          elements: [
+            new ElementExample('Half-width button', 'button--halfwidth', 'button'),
+            new ElementExample('Full-width button', 'button--fullwidth', 'button')
+          ]
+        },
+        buttonStyle: {
+          title: 'Button style',
+          commonClasses: 'button button--medium',
+          elements: [
+            new ElementExample('Simple button', 'button--simple button--primary', 'button'),
+            new ElementExample('Ghost button', 'button--ghost button-ghost--primary', 'button')
+          ]
+        },
+        buttonGroup: {
+          title: 'Button grouping',
+          description: 'Set a grouping container for buttons.  This will add the correct padding etc.',
+          commonClasses: 'button button--simple button--primary button--medium',
+          elements: [
+            new ElementExample('Button 1', '', 'button'),
+            new ElementExample('Button 2', '', 'button')
+          ]
+        }
       };
     }
   };
